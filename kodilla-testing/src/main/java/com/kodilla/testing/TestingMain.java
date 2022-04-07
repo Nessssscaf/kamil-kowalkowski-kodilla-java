9e22c12c8f51af9250a12a13ee1617a02644399d
Kamil Kowalkowski <https://github.com/Nessssscaf/kamil-kowalkowski-kodilla-java.git>
2022-04-07 22:12:49 +0200

    Test prostego kalkulatora

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,41 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
 public class TestingMain {
+
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // test dodawania
+        Calculator addCalculator = new Calculator(10 ,5);
+
+        int actionResult = addCalculator.getA() + addCalculator.getB();
+
+        if (actionResult == 15) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        // test odejmowania
+        Calculator subtractCalculator = new Calculator(10 ,5);
+
+        int actionResult2 = addCalculator.getA() - addCalculator.getB();
+
+        if (actionResult2 == 5) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
     }
 }
