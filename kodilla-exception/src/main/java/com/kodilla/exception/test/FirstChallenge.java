d3d7b7d3e755127edcf74ba276408c1f890d0169
Kamil Kowalkowski <https://github.com/Nessssscaf/kamil-kowalkowski-kodilla-java.git>
2022-05-09 20:58:13 +0200

    try! catch! finally! 2

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -2,18 +2,11 @@ package com.kodilla.exception.test;
 
 public class FirstChallenge {
 
-    public void divide(double a, double b) throws ArithmeticException {
-        try {
-            if (b == 0) {
-                throw new ArithmeticException();
-            }
-            double result = a / b;
-            System.out.println(result);
-        } catch (ArithmeticException e) {
-            System.out.println("Oh no! Cannot be divided by zero! Error:" + e);
-        } finally {
-            System.out.println("End of challenge");
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
         }
+        return a / b;
     }
 
     /**
@@ -22,6 +15,14 @@ public class FirstChallenge {
      */
     public static void main(String[] args) {
         FirstChallenge firstChallenge = new FirstChallenge();
-        firstChallenge.divide(3, 0);
+
+        try {
+            double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("Oh no! Cannot be divided by zero! Error: " + e);
+        } finally {
+            System.out.println("End of challenge");
+        }
     }
 }
